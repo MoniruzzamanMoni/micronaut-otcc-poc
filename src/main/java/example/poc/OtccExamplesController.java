8c435267fd1eff1e1f73bb1244da491719a878e1
Md. Moniruzzaman <moni.return@gmail.com>
2023-05-07 04:28:11 +0600

    adding test home page

--- src/main/java/example/poc/OtccExamplesController.java
+++ src/main/java/example/poc/OtccExamplesController.java
@@ -5,13 +5,12 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
-import io.micronaut.http.cookie.Cookie;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller("/")
+@Controller
 public class OtccExamplesController {
     private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
     private final AppConfig appConfig;
@@ -20,21 +19,31 @@ public class OtccExamplesController {
         this.appConfig = appConfig;
     }
 
+    List<String> links = Arrays.asList(
+            "http://localhost:8080/otcc/archive/cta/printversion/cta_ar_2022-04-15.xml",
+            "http://localhost:8080/otcc/archive/cta/printversion/cta_br_2022-07-01.xml",
+            "http://localhost:8080/otcc/archive/cta/printversion/cta_fi_2022-07-28.xml",
+            "http://localhost:8080/otcc/archive/gtha/printversion/gtha_ar_2022-07-01.xml",
+            "http://localhost:8080/otcc/archive/gtha/printversion/gtha_br_2020-11-01.xml",
+            "http://localhost:8080/otcc/archive/gtha/printversion/gtha_ca_2021-04-26.xml"
+    );
+    String body = "<h3>OTCC Example Links</h3><hr>" + links.stream()
+            .map(l -> "<a href=\"%s\">%s</a>".formatted(l, l))
+            .collect(Collectors.joining("<br>"));
+
     @Get(uri="/", produces = MediaType.ALL)
     public HttpResponse<String> getExampleLinks() {
-        List<String> links = Arrays.asList(
-                "http://localhost:8080/otcc/archive/cta/printversion/cta_ar_2022-04-15.xml",
-                "http://localhost:8080/otcc/archive/cta/printversion/cta_br_2022-07-01.xml",
-                "http://localhost:8080/otcc/archive/cta/printversion/cta_fi_2022-07-28.xml",
-                "http://localhost:8080/otcc/archive/gtha/printversion/gtha_ar_2022-07-01.xml",
-                "http://localhost:8080/otcc/archive/gtha/printversion/gtha_br_2020-11-01.xml",
-                "http://localhost:8080/otcc/archive/gtha/printversion/gtha_ca_2021-04-26.xml"
-        );
-        String body = "<h3>OTCC Example Links</h3><hr>" + links.stream()
-                .map(l -> "<a href=\"%s\">%s</a>".formatted(l, l))
-                .collect(Collectors.joining("<br>"));
         return HttpResponse.ok(body)
                 .contentLength(body.length())
                 .contentType(CONTENT_TYPE);
     }
+
+    @Get(uri="/{authKey}", produces = MediaType.ALL)
+    public HttpResponse<String> getExampleLinks(@PathVariable String authKey) {
+        return HttpResponse.ok(body)
+                .contentLength(body.length())
+                .contentType(CONTENT_TYPE)
+                .header("set-cookie", "DEV_IBFD_SESSION=%s".formatted(authKey));
+    }
+
 }
